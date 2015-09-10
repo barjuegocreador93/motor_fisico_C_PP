a5df0905bc4bbee5d59df6e7e7aa5736a0d04189
Camilo Barbosa <barbosa93cg@gmail.com>
2015-09-10 10:31:03 -0500

    obj

--- Template-Allegro/Allegro/AllegroObj.h
+++ Template-Allegro/Allegro/AllegroObj.h
@@ -1,1 +1,16 @@
 
+typedef BITMAP *img;/**Imagenes por direcciones de memoria*/
+/**Vector de imagenes*/
+
+class setBitmap
+{
+   public
+     setBitmap();
+     ~setBitmap();
+     void create_bitmap();
+     
+   protected
+      
+   private
+     vector<img>vImg; 
+};
